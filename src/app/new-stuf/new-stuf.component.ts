89261196f9852471cd770fa2c2829e935c3606d1
mfrandes <frandesmyrce@gmail.com>
2019-06-24 13:02:56 +0300

    creating 2 new components on my own

---
+++ src/app/new-stuf/new-stuf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-stuf',
+  templateUrl: './new-stuf.component.html',
+  styleUrls: ['./new-stuf.component.css']
+})
+export class NewStufComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
