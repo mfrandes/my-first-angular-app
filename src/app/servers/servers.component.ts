5361cfbeeb1c0275ff95505c84e291853c5e33c4
mfrandes <frandesmyrce@gmail.com>
2019-06-24 17:38:29 +0300

    Html binding proprieties

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,15 +4,20 @@ import { Component, OnInit } from '@angular/core';
   //selector: '[app-servers]', // selecting element by tag
   //selector: '.app-servers', // selecting element by class; selecting by Id not working!
   selector: 'app-servers', //selecting element by elements name Recomended!
-  template: `
+  /*template: `
   <app-server></app-server>
-  <app-server></app-server>`,
+  <app-server></app-server>`,*/
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
-
+  allowNewServer = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000);
+   }
+  
   ngOnInit() {
   }
 
