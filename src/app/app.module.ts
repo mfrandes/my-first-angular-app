1149f052dbdf226c0f5cba663fde247d938b8d3f
mfrandes <frandesmyrce@gmail.com>
2019-06-25 16:18:36 +0300

    creating new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
